fefbfcfeece74065edb77ab39295046f9d6e09fd
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-06-10 16:33:56 -0300

    Agregue construct, get y set para la clase Pasajero

--- Clases/Pasajero.php
+++ Clases/Pasajero.php
@@ -2,7 +2,72 @@
 
 class Pasajero {
 
-     
-}
+    private $nombre;
+    private $apellido;
+    private $nroDoc;
+    private $telefono;
+    private $mensajeoperacion;
 
+    public function __construct(){
+
+		$this->nombre = "";
+		$this->apellido = "";
+		$this->nroDoc = "";
+		$this->telefono = "";
+
+	}
+
+    // Metodo Cargar Datos
+    public function cargar($nomb, $apelli, $nroD, $telef){
+		
+		$this->setNombre($nomb);
+		$this->setApellido($apelli);
+		$this->setNrodoc($nroD);
+        $this->setTelefono($telef);
+
+	}
+
+    // Metodo Get------------------------------------------------------------------
+	public function getNombre() {
+		return $this->nombre;
+	}
+
+	public function getApellido() {
+		return $this->apellido;
+	}
+
+	public function getNroDoc() {
+		return $this->nroDoc;
+	}
+
+	public function getTelefono() {
+		return $this->telefono;
+	}
+
+	public function getMensajeoperacion() {
+		return $this->mensajeoperacion;
+	}
+
+    // Metodo Set------------------------------------------------------------------
+	public function setNombre($nuevoNombre) {
+		$this->nombre = $nuevoNombre;
+	}
+
+	public function setApellido($nuevoApellido) {
+		$this->apellido = $nuevoApellido;
+	}
+
+	public function setNroDoc($nuevoNroDoc) {
+		$this->nroDoc = $nuevoNroDoc;
+	}
+
+	public function setTelefono($nuevoTelefono) {
+		$this->telefono = $nuevoTelefono;
+	}
+
+	public function setMensajeoperacion($nuevoMensajeOperacion) {
+		$this->mensajeoperacion = $nuevoMensajeOperacion;
+	}
+
+}
 ?>
