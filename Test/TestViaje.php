52e519e8e1e1df4be0f09029394401e39c0f11a3
Lian Ivan Sinchez <liam.sinchez00@gmail.com>
2024-06-12 18:48:51 -0300

    haciendo funcionar la bd (intentando)

--- Test/TestViaje.php
+++ Test/TestViaje.php
@@ -1,2 +1,19 @@
 <?php
 
+include_once "../Clases/Persona.php";
+include_once "../Clases/ResponsableV.php";
+include_once "../Clases/BaseDatos.php";
+
+$res = new ResponsableV();
+
+$res->cargar(123, 23423423, "Lian", "Sinchez", 4534534534, 34534543543);
+
+$respuesta = $res->insertar();
+
+if($respuesta){
+    echo "Funcionando";
+}else{
+    echo "No funciona :(";
+}
+
+echo $res;
