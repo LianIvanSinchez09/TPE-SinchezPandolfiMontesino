79aecefd18dd2fdd3b72c7a9ca25de888633ac4c
lautyMonte <lausanmonte@gmail.com>
2024-06-12 20:37:48 -0300

    cambie cosas del sql

--- bdviajes.sql
+++ bdviajes.sql
@@ -1,53 +1,76 @@
-CREATE DATABASE bdviajes; 
-    /*los FOREIGN KEY () son utilizados como puentes entre las tablas para saber en que parte estan guardada su informacion completa
-    Por ejemplo: Persona va tener un id y toda su informacion, cuando se quiere crear el responsable tendra como foranea el id,
-    gracias a ese podremos saber donde esta su informacion dentro de la tabla persona
-    Hay que hacer solo un test
-    Al querer crear un hijo, primero debo crarlo en el padre y, si se puede, ahi recien creo al hijo con todos sus datos*/
-CREATE TABLE empresa(
-    idempresa bigint AUTO_INCREMENT,
-    enombre varchar(150),
-    edireccion varchar(150),
-    PRIMARY KEY (idempresa)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-    CREATE TABLE persona (
-    documento VARCHAR(15),
-    nombre VARCHAR(150),
-    apellido VARCHAR(150),
+/* CREATE DATABASE bdviajes; */
+CREATE TABLE persona (
+    documento varchar(15),
+    nombre varchar(150), 
+    apellido varchar(150), 
     PRIMARY KEY (documento)
     )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE responsable (
-    documento VARCHAR(15),
-    numeroempleado bigint AUTO_INCREMENT,
-    numerolicencia bigint, 
-    PRIMARY KEY (numeroempleado),
-    FOREIGN KEY (documento) REFERENCES persona (documento) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE personaResponsable (    
+    documento varchar(15),
+    numeroEmpleado bigint AUTO_INCREMENT,    
+    numeroLicencia bigint,
+    PRIMARY KEY (numeroEmpleado),
+    FOREIGN KEY (documento) REFERENCES persona (documento)
+    ON UPDATE CASCADE ON DELETE CASCADE
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1;	
+
+    CREATE TABLE empresa (
+    idEmpresa bigint AUTO_INCREMENT,
+    nombre varchar(150),
+    direccion varchar(150),
+    PRIMARY KEY (idEmpresa)
     )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-    CREATE TABLE pasajero (
-    documento varchar(15),
-	ptelefono int, 
-	idviaje bigint,
-    PRIMARY KEY (documento),
-    FOREIGN KEY (documento) REFERENCES persona (documento) 
-    ON UPDATE CASCADE ON DELETE CASCADE
-	FOREIGN KEY (idviaje) REFERENCES viaje (idviaje)/*se hace por una relacion de 1:1,para saber en que viaje esta el pasajero*/	
-    
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-	
     CREATE TABLE viaje (
-    idviaje bigint AUTO_INCREMENT, /*codigo de viaje*/
-	vdestino varchar(150),
-    vcantmaxpasajeros int,
-	idempresa bigint,
-    numeroempleado bigint,
-    vimporte float,
-    PRIMARY KEY (idviaje),
-    FOREIGN KEY (idempresa) REFERENCES empresa (idempresa),
-	FOREIGN KEY (numeroempleado) REFERENCES responsable (numeroempleado)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1;
+    idViaje bigint AUTO_INCREMENT,
+	destino varchar(150),
+    cantMaxPasajeros int,
+    numeroEmpleadoResponsable bigint,
+    idEmpresa bigint,
+    costoIndividualPasajero float,
+    importeTotalPasajeros float,
+    PRIMARY KEY (idViaje),
+    FOREIGN KEY (idEmpresa) REFERENCES empresa (idEmpresa),
+    FOREIGN KEY (numeroEmpleadoResponsable) REFERENCES personaResponsable (numeroEmpleado)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1;
+    
+    CREATE TABLE pasajero (
+    idPasajero bigint AUTO_INCREMENT,
+    documento varchar(15),	
+	idViajeiaje bigint,
+    telefono int,
+    numeroAsiento varchar(4),
+    numeroTickect varchar(10),
+    PRIMARY KEY (idPasajero),
+    FOREIGN KEY (documento) REFERENCES persona (documento)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (idViajeiaje) REFERENCES viaje (idViaje)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1; 
+
+    CREATE TABLE standard (
+    idPasajero bigint,
+    PRIMARY KEY (idPasajero),
+    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero)
+    ON UPDATE CASCADE ON DELETE CASCADE	
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+    CREATE TABLE vip (
+    idPasajero bigint,	
+    numeroViajeroFecuente int,
+    cantidadMillas int,
+    PRIMARY KEY (idPasajero),
+    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero)
+    ON UPDATE CASCADE ON DELETE CASCADE	
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+    CREATE TABLE especial (
+    idPasajero bigint,   
+    requiereSillasRuedas varchar(10),/* No seria mejor una variable que se llame necesidad y de ahi escribir la necesidad?*/
+    requiereAsistencia varchar(10),
+    requiereComidaEsecial varchar(10),
+    PRIMARY KEY (idPasajero),
+    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero) 
+    ON UPDATE CASCADE ON DELETE CASCADE	   
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
   
