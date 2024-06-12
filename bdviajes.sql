354ff930bb7ce7952e5f35c9caf17ff67a4ca5dc
Lian Ivan Sinchez <liam.sinchez00@gmail.com>
2024-06-12 16:57:31 -0300

    empezando la clase responsableV

--- bdviajes.sql
+++ bdviajes.sql
@@ -24,26 +24,25 @@ CREATE TABLE responsable (
 
     CREATE TABLE pasajero (
     pNroDoc varchar(15),
-	ptelefono int, 
-	idviaje bigint,
+    ptelefono int, 
+    idviaje bigint,
     PRIMARY KEY (pNroDoc),
     FOREIGN KEY (pNroDoc) REFERENCES persona (nroDoc) 
     ON UPDATE CASCADE ON DELETE CASCADE
-	FOREIGN KEY (idviaje) REFERENCES viaje (idviaje)	
-    
+    FOREIGN KEY (idviaje) REFERENCES viaje (idviaje)
+
     )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-	
-    CREATE TABLE viaje (
-    idviaje bigint AUTO_INCREMENT, /*codigo de viaje*/
-	vdestino varchar(150),
+
+CREATE TABLE viaje (
+    idviaje bigint AUTO_INCREMENT, /codigo de viaje/
+    vdestino varchar(150),
     vcantmaxpasajeros int,
-	idempresa bigint,
+    idempresa bigint,
     rnumeroempleado bigint,
     vimporte float,
     PRIMARY KEY (idviaje),
     FOREIGN KEY (idempresa) REFERENCES empresa (idempresa),
-	FOREIGN KEY (rnumeroempleado) REFERENCES responsable (rnumeroempleado)
+    FOREIGN KEY (rnumeroempleado) REFERENCES responsable (rnumeroempleado)
     ON UPDATE CASCADE
     ON DELETE CASCADE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1;
-  
