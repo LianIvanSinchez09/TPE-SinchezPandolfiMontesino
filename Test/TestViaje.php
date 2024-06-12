1eeb1c9d95ba1c7660aa26877f30ba7fd2eed4ed
Lian Ivan Sinchez <liam.sinchez00@gmail.com>
2024-06-12 19:50:15 -0300

    funcionando base datos

--- Test/TestViaje.php
+++ Test/TestViaje.php
@@ -6,7 +6,7 @@ include_once "../Clases/BaseDatos.php";
 
 $res = new ResponsableV();
 
-$res->cargar(21312312312, "Lian", "Sinchez", 123, 456);
+$res->cargar(696969, "Liana", "Sanchez", 123, 456);
 
 $respuesta = $res->insertar();
 
