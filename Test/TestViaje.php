e55d8241a8ef98285d9b567105976d28afe14dbe
lautyMonte <lausanmonte@gmail.com>
2024-06-12 19:51:49 -0300

    a

--- Test/TestViaje.php
+++ Test/TestViaje.php
@@ -1,19 +1,2 @@
 <?php
 
-include_once "../Clases/Persona.php";
-include_once "../Clases/ResponsableV.php";
-include_once "../Clases/BaseDatos.php";
-
-$res = new ResponsableV();
-
-$res->cargar(21312312312, "Lian", "Sinchez", 123, 456);
-
-$respuesta = $res->insertar();
-
-if($respuesta){
-    echo "Funcionando";
-}else{
-    echo "No funciona :(";
-}
-
-echo $res;
