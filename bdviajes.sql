022142e7472b9a78be9c58f082352037795d01d1
lautyMonte <lausanmonte@gmail.com>
2024-06-14 15:25:02 -0300

    saque las tablas de los diferentes tipos de pasajero

--- bdviajes.sql
+++ bdviajes.sql
@@ -47,30 +47,4 @@ CREATE TABLE personaResponsable (
     ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (idViajeiaje) REFERENCES viaje (idViaje)
     )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1; 
-
-    CREATE TABLE standard (
-    idPasajero bigint,
-    PRIMARY KEY (idPasajero),
-    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero)
-    ON UPDATE CASCADE ON DELETE CASCADE	
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-    CREATE TABLE vip (
-    idPasajero bigint,	
-    numeroViajeroFecuente int,
-    cantidadMillas int,
-    PRIMARY KEY (idPasajero),
-    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero)
-    ON UPDATE CASCADE ON DELETE CASCADE	
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-    CREATE TABLE especial (
-    idPasajero bigint,   
-    requiereSillasRuedas varchar(10),/* No seria mejor una variable que se llame necesidad y de ahi escribir la necesidad?*/
-    requiereAsistencia varchar(10),
-    requiereComidaEsecial varchar(10),
-    PRIMARY KEY (idPasajero),
-    FOREIGN KEY (idPasajero) REFERENCES pasajero (idPasajero) 
-    ON UPDATE CASCADE ON DELETE CASCADE	   
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
   
