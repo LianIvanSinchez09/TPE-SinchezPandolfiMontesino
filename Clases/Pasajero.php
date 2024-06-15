24a4897eee5386f1bbfb0968f06b0acaed8c395c
Lian Ivan Sinchez <liam.sinchez00@gmail.com>
2024-06-15 17:46:42 -0300

    el metodo insertar anda mal

--- Clases/Pasajero.php
+++ Clases/Pasajero.php
@@ -2,14 +2,14 @@
 
 class Pasajero extends Persona {
 	private $idpasajero;
-	private $idviaje;
+	private $objViaje;
     private $telefono;
     private $mensajeoperacion;
 
     public function __construct(){
 		parent::__construct();
 		$this->idpasajero=0;
-		$this->idviaje="";
+		$this->objViaje = new Viaje();
 		$this->telefono = "";
 
 	}
@@ -20,11 +20,11 @@ class Pasajero extends Persona {
 		parent::cargar($param);
 		$this->setIdPasajero($param['idpasajero'] );
         $this->setTelefono($param['telef'] );*/
-  	public function cargar($NroD,$Nom,$Ape, $idviaje=null,$telef = null){
+  	public function cargar($NroD,$Nom,$Ape, $objViaje=null,$telef = null){
 		parent::cargar($NroD,$Nom,$Ape);
 		$this->setIdPasajero(0);
         $this->setTelefono($telef);
-		$this->setIdViaje($idviaje);
+		$this->setobjViaje($objViaje);
 	}
 
     // <--------------Metodo Get------------------------------------------------------->
@@ -34,8 +34,8 @@ class Pasajero extends Persona {
 	public function getTelefono() {
 		return $this->telefono;
 	}
-	public function getIdViaje(){
-		return $this->idviaje;
+	public function getobjViaje(){
+		return $this->objViaje;
 	}
 
 	public function getMensajeoperacion() {
@@ -51,31 +51,28 @@ class Pasajero extends Persona {
 		$this->telefono = $nuevoTelefono;
 	}
 
-	public function setIdViaje($idviaje){
-		$this->idviaje=$idviaje;
+	public function setobjViaje($objViaje){
+		$this->objViaje=$objViaje;
 	}
 
 	public function setMensajeoperacion($nuevoMensajeOperacion) {
 		$this->mensajeoperacion = $nuevoMensajeOperacion;
 	}
 
-	//<--------------------Metodos----------------------->
-
+	//<--------------------Metodos-- --------------------->
 	public function insertar(){
 		$base=new BaseDatos();
 		$resp= false;
-		$consultaInsertar="INSERT INTO personaresponsable(documento, telefono) 
-			VALUES (".$this->getdocumento().",'".$this->getTelefono()."')";
+		$consultaInsertar="INSERT INTO pasajero(idPasajero, documento, objViaje, telefono) 
+			VALUES ('".$this->getIdPasajero()."','".$this->getdocumento()."','" . $this->getobjViaje()->getIdViaje() . "','" . $this->getTelefono() . "')";
 		
 		if($base->Iniciar()){
 
 			if($id = $base->devuelveIDInsercion($consultaInsertar)){
-                $this->setIdPasajero($id);
+				$this->setIdPasajero($id);
 			    $resp=  true;
-
 			}	else {
 					$this->setmensajeoperacion($base->getError());
-					
 			}
 
 		} else {
@@ -119,5 +116,10 @@ class Pasajero extends Persona {
 		}
 		return $resp; 
 	}
+
+	public function __toString(){
+		return parent::__toString() . "\nobjViaje: " . $this->getobjViaje() . "\nTelefono: " . $this->getTelefono();
+	}
+
 }
 ?>
