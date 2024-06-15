4120ba1c9a3f6b3a33a16438e15a648d82a82f0d
lautyMonte <lausanmonte@gmail.com>
2024-06-15 19:52:43 -0300

    es una prueba

--- Test/TestViaje.php
+++ Test/TestViaje.php
@@ -2,10 +2,10 @@
 
 include_once "../Clases/Persona.php";
 include_once "../Clases/ResponsableV.php";
-include_once "../Clases/Pasajero.php";
 include_once "../Clases/Empresa.php";
 include_once "../Clases/Viaje.php";
 include_once "../Clases/BaseDatos.php";
+include_once "../Clases/Pasajero.php";
 
 //si no tiene cargado a la persona antes de cargarlo en responsable, no funciona
 $res = new Persona();
@@ -15,12 +15,12 @@ $res1->cargar(44323057, "Lian", "Sinchez",22,22);
 $respuesta = $res->insertar();
 $respuesta1 = $res1->insertar();
 
-//prueba con empresa
+// //prueba con empresa
 $emp = new Empresa();
 $emp->cargar(1, "koko", "Neuquen Capital");
 $respuesta2 = $emp->insertar();
 
-//prueba con viaje 
+// //prueba con viaje 
 $viaje = new Viaje();
 $viaje->cargar(1, "Cipolletti", 20, $res1, $emp, 1000);
 $respuesta3 = $viaje->insertar();
@@ -31,11 +31,20 @@ if($respuesta){
     echo "\nNo funciona :(\n";
 }
 
+$persona2 = new Persona();
+$persona2->cargar(34534534, "Francisco", "Pandolfi");
+$persona2->insertar();
+$pasajero = new Pasajero();
+$pasajero->cargar(34534534, "Francisco", "Pandolfi", $viaje , 123456);
+$pasajero->insertar();
+
 echo "\nClase Persona";
 echo $res;
 echo "\nClase Persona Responsable";
 echo $res1;
 echo "\nClase Empresa";
 echo $emp;
+echo "\nClase Pasajero";
+echo $pasajero;
 echo "\nClase Viaje";
 echo $viaje;
