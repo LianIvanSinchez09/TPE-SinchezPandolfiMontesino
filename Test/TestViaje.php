ade669af0d50751b8c1fa7d3131d5b39ba09081f
lautyMonte <lausanmonte@gmail.com>
2024-06-17 16:17:24 -0300

    algp

--- Test/TestViaje.php
+++ Test/TestViaje.php
@@ -28,36 +28,6 @@ $pasajero = new Pasajero();
 $pasajero->cargar(34534534, "Francisco", "Pandolfi", 1, $viaje, 2995920034);
 $pasajero->insertar();
 
-/*echo "\nClase Persona 1";
-echo $res;
-echo "\nClase Persona 2";
-echo $persona2;
-echo "\nClase Persona Responsable";
-echo $res1;
-echo "\nClase Empresa";
-echo $emp;
-echo "\nClase Viaje";
-echo $viaje;
-echo "\nClase Pasajero";
-echo $pasajero;
-
-$pasajero->setTelefono("2995551234");
-
-if ($pasajero->modificar()) {
-    echo "\nDatos del pasajero modificados correctamente.\n";
-} else {
-    echo "\nError al modificar los datos del pasajero: " . $pasajero->getMensajeoperacion() . "\n";
-}
-
-echo "\nClase Pasajero modificada";
-echo $pasajero;
-
-if($pasajero->hayPasajesDisponibles()){
-    echo "Hay pasajes disponibles";
-}else{
-    echo "No disponible";
-}*/
-
 //<-------------------------ACA ESTA--------------------->
 /**
  * menu de opciones para hacer el test
@@ -203,6 +173,7 @@ do{
     $opcion = trim(fgets(STDIN));
     $otroPersona=new Persona();
     $otroPasajero=new Pasajero();//hago esto para poder acceder a los metodos de pasajero
+    $otroResponsable=new ResponsableV();
     switch($opcion){
         case 1:
             $listaPasajero=$otroPasajero->listar();
@@ -309,6 +280,10 @@ do{
                 default:
                     echo "Opcion no existente";
                 break;
+        case 5:
+            echo "Ingrese";
+            ;break;
+        case 6:;break;
     }
     echo "\nDesea hacer otra cosa? s/n\n";
     $desicion = trim(fgets(STDIN));
